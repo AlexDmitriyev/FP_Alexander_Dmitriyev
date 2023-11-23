8a409cf1496fa04ea880887c54d6e4134b94e759
Alexander G. Dmitriyev <admitriyev@riverlogic.com>
2023-11-23 14:36:26 +0400

    -Added processing errors.

--- scripts/weather_script.js
+++ scripts/weather_script.js
@@ -12,19 +12,34 @@ function update_weather_report(city) {
 	let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=${lang}&appid=${key}`;
     
 	fetch(url)
-		.then(response => response.json())
-	.then((obj) => {
-                const icon_id = obj.weather[0].icon;
-		const img_src = `https://openweathermap.org/img/wn/${icon_id}@2x.png`;
-		document.getElementById("weather_icon").src = img_src
-		document.getElementById("weather_desc_id").innerHTML=''+ obj.weather[0].main; 
-		document.getElementById("temp_id").innerHTML='<b><i>'+ obj.main.temp + '°C</i></b> (мин. <b><i>' + 
-			obj.main.temp_min + '°C</i></b>, макс. <b><i>' + obj.main.temp_max + '°C</i></b>, ощущается как <b><i>' + obj.main.feels_like + '°C</i></b>)'; 
-		document.getElementById("wind_id").innerHTML='скорость <b><i>'+ obj.wind.speed + ' м/с</i></b>, направление <b><i>' + 
-			obj.wind.deg + '°</i></b>'; 
-		document.getElementById("clouds_id").innerHTML='<b><i>'+ obj.clouds.all + '%</i></b>'; 
-		document.getElementById("title_id").innerHTML=`Погода в г. ${city}`;
-		}
+		.then(response => {
+		    console.log(response.status); // Will show you the status
+		    if (!response.ok) {
+		        throw new Error('город не найден');
+		    }
+		    
+                    return response.json();
+		})
+		.then(obj => {
+	        	        const icon_id = obj.weather[0].icon;
+				const img_src = `https://openweathermap.org/img/wn/${icon_id}@2x.png`;
+				document.getElementById("weather_icon").src = img_src
+				document.getElementById("weather_desc_id").innerHTML=''+ obj.weather[0].main; 
+				document.getElementById("temp_id").innerHTML='<b><i>'+ obj.main.temp + '°C</i></b> (мин. <b><i>' + 
+					obj.main.temp_min + '°C</i></b>, макс. <b><i>' + obj.main.temp_max + '°C</i></b>, ощущается как <b><i>' + obj.main.feels_like + '°C</i></b>)'; 
+				document.getElementById("wind_id").innerHTML='скорость <b><i>'+ obj.wind.speed + ' м/с</i></b>, направление <b><i>' + 
+					obj.wind.deg + '°</i></b>'; 
+				document.getElementById("clouds_id").innerHTML='<b><i>'+ obj.clouds.all + '%</i></b>'; 
+				document.getElementById("title_id").innerHTML=`Погода в г. ${city}`;
+				document.getElementById("err_text").style.display = 'none';
+				document.getElementById("weather_icon_group_id").style.display = 'block';
+				document.getElementById("weather_details_group_id").style.display = 'block';			
+			})
+	.catch( err => {
+				document.getElementById("weather_icon_group_id").style.display = 'none';
+				document.getElementById("weather_details_group_id").style.display = 'none';
 
-		);
+				document.getElementById("err_text").innerHTML = 'Ошибка при выполнении запроса: '+ err.message; 
+				document.getElementById("err_text").style.display = 'block';
+		});
 }
